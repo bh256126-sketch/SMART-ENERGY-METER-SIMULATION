772a97cfc54950094e3792e7cc420e98c0bfc749
bh256126-sketch <bh256126@students.cavendish.ac.ug>
2026-04-28 12:18:47 +0000

    added code for ESP32 and linking to the Blynk Platform

--- smart-energy-meter-simulation.ino
+++ smart-energy-meter-simulation.ino
@@ -1,20 +1,38 @@
-#include <LiquidCrystal.h>
 
-#define SIGNAL A0
-#define GREEN 8      // updated
-#define YELLOW 7     // updated
-#define RED 13       // unchanged
+#define BLYNK_TEMPLATE_ID "TMPL2vXLMLiEc"
+#define BLYNK_TEMPLATE_NAME "Smart Energy Meter"
+#define BLYNK_AUTH_TOKEN "qmrenGOffMXq6xxncFyPOXif1MHE12gF"
+
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include <WiFi.h>
+#include <BlynkSimpleEsp32.h>
+
+char ssid[] = "Wokwi-GUEST";
+char pass[] = "";
+
+#define SIGNAL 34
+#define GREEN 13      // updated
+#define YELLOW 14     // updated
+#define RED 27
+
+#define I2C_SDA 21
+#define I2C_SCL 22      // unchanged
 
 float tariff = 756.2; 
 float current = 0.5;
 unsigned long previousTime = 0;
-float energy;
+float energy = 0;
 
-// Wokwi LCD1602 pin mapping
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+//Set LCD address
+LiquidCrystal_I2C lcd(0x27, 16, 2); 
 
 void setup() {
-  Serial.begin(9600);
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
+  Serial.println("Hello, ESP32!");
+  // Serial.begin(9600);
   pinMode(SIGNAL, INPUT);
   previousTime = millis();
 
@@ -22,17 +40,26 @@ void setup() {
   pinMode(YELLOW, OUTPUT);
   pinMode(RED, OUTPUT);
 
-  lcd.begin(16, 2);
-  lcd.clear();
+  // Initialize I2C with custom pins
+  Wire.begin(I2C_SDA, I2C_SCL);
+
+  // Initialize LCD
+  lcd.init();
+  lcd.backlight();
   lcd.setCursor(0, 0);
   lcd.print("Energy Monitor");
+
   delay(1000);
   lcd.clear();
 }
 
 void loop() {
+  // put your main code here, to run repeatedly:
+  delay(10); // this speeds up the simulation
+  Blynk.run();
+
   int sensorValue = analogRead(SIGNAL);
-  float voltage = sensorValue * (5.0 / 1023.0);
+  float voltage = sensorValue * (3.3 / 4095.0);
   float power = voltage * current;
 
   unsigned long currentTime = millis();
@@ -45,34 +72,41 @@ void loop() {
   String statusText = "";
 
   // LED + STATUS INTERPRETATION
-  if (power < 1) {
+  if (power < 0.5) {
     digitalWrite(GREEN, HIGH);
     digitalWrite(YELLOW, LOW);
     digitalWrite(RED, LOW);
-    statusText = "LOW usage     ";
+    statusText = "LOW ";
   } 
-  else if (power < 2) {
+  else if (power < 1.2) {
     digitalWrite(GREEN, LOW);
     digitalWrite(YELLOW, HIGH);
     digitalWrite(RED, LOW);
-    statusText = "MEDIUM usage  ";
+    statusText = "MEDIUM ";
   } 
   else {
     digitalWrite(GREEN, LOW);
     digitalWrite(YELLOW, LOW);
     digitalWrite(RED, HIGH);
-    statusText = "HIGH usage    ";
+    statusText = "HIGH ";
   }
 
   // LCD OUTPUT (interprets LED state)
   lcd.setCursor(0, 0);
-  lcd.print("Status:");
   lcd.print(statusText);
+  lcd.print(energy);
+  lcd.print("KWh");
 
   lcd.setCursor(0, 1);
   lcd.print("Cost:");
   lcd.print(cost, 1);
   lcd.print(" UGX   ");
+
+  // send to dashboard
+  Blynk.virtualWrite(V0, power);
+  Blynk.virtualWrite(V1, energy);
+  Blynk.virtualWrite(V2, cost);
+  Blynk.virtualWrite(V3, statusText);
 
   delay(1000);
 }
